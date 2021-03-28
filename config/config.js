4e79c8c2a78a8a33cfb709734ca6fb28e441aeae
jhLim97 <wnsgur2749@naver.com>
2021-03-28 19:54:13 +0900

    feat: / 기본경로 수정

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ module.exports = {
     db_url:'',
     db_schemas: [],
     route_info: [
-        {file:'./scanner', path:'/scanner', method:'scanner', type:'get'}
+        {file:'./scanner', path:'/', method:'scanner', type:'get'}
     ]                           
 };
--- router/scanner.js
+++ router/scanner.js
@@ -1,7 +1,7 @@
 
 
 var scanner = function(req, res) {
-    console.log('/scanner 패스로 GET 요청됨.');
+    console.log('/ 패스로 GET 요청됨.');
 
     res.render('scanner');
 }
