e6415b1c18c9bffa59f9709f0e978194c566365c
jhLim97 <wnsgur2749@naver.com>
2021-05-11 14:11:35 +0900

    feat: 얼굴인식 api 추가

--- router/faceRecognition.js
+++ router/faceRecognition.js
@@ -4,16 +4,18 @@ const recognition = (req, res) => {
     console.log('/process/recognition으로 POST 요청됨.');
 
     const { spawn } = require('child_process');
-    const pyLibrary = spawn('python', ['lib/test.py', 'afwfseg', 'lim-yoon-jeong']);
+    const pyLibrary = spawn('python', ['lib/FaceRecognition.py']);
 
     pyLibrary.stdout.on('data', (result) => {
         console.log(result.toString());
         res.write(result);
+        res.end();
     });
 
     pyLibrary.stderr.on('data', (result) => {
         console.log(result.toString());
         res.write(result);
+        res.end();
     });
 }
 
