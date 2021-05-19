db4a15b1c014ba65a980fb895862e97b173dd109
jhLim97 <wnsgur2749@naver.com>
2021-05-19 22:38:38 +0900

    feat: 유저 인터페이스 구현

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
 module.exports = {
     server_port:3300,
+    admin_id:'lim-yoon-jeong',
+    admin_password:'2021',
     route_info: [
-        {file:'./scanner', path:'/', method:'scanner', type:'get'},
+        {file:'./index', path:'/', method:'index', type:'get'},
+        {file:'./admin', path:'/admin', method:'adminLogin', type:'post'},
+        {file:'./admin', path:'/admin/list', method:'list', type:'get'},
+        {file:'./admin', path:'/admin/show', method:'show', type:'get'},
         {file:'./attendance', path:'/process/submit', method:'submit', type:'post'},
         {file:'./attendance', path:'/process/listAll', method:'listAll', type:'post'},
         {file:'./faceRecognition', path:'/process/recognition', method:'recognition', type:'post'}
